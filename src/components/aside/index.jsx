130be4e78e69884e94718b9cb47103a8d3e255c0
scarlletValentim <scarlletrvs34@gmail.com>
2023-03-24 16:31:49 -0300

    feat: create page perfil

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -1,5 +1,7 @@
-import { AsideStyle } from "../style"
-import logo from "../assets/logo.png"
+import { AsideStyle } from  "./style"
+import logo from "../../assets/logo.png"
+import { House, DeviceMobile, User, DotsThreeCircle, Siren } from "@phosphor-icons/react"
+
 
 export const Aside = () => {
     return(
@@ -7,29 +9,39 @@ export const Aside = () => {
         <AsideStyle>
             <img src={logo} alt="" />
             
-            <a href="#"  class="btn-perfil">
-              <h3>usuário </h3>
+            <div>
+              <img href="#" className="img-perfil"></img>
+              <h3 className="h3-perfil">usuário </h3>
               <h5>@usuário</h5>
-            </a>
+            </div>
          
 
-            <a href="#"   class="btn-inicio">
-                <h3> Inicio</h3> </a>
+            <a href="#"   classname="btn-inicio">
+                <House size={24} />
+                <h3> Inicio</h3>
+            </a>
 
             <a href="#">
-                <h3> Feed </h3> </a>
+                <DeviceMobile size={24}/>
+                <h3> Feed </h3>
+             </a>
 
             <a href="#" >
+                <User size={24} />
                 <h3> Pefil</h3> </a>
 
                 <a href="#" >
+                    <DotsThreeCircle size={24}/>
                 <h3> Configurações</h3> </a>
-
+                
+                <div className="button-div">
                 <button>
                     Alertar
                 </button>
-
-
+                <button className="btn-mobile">
+                    <Siren size={24}/>
+                </button>
+                </div>
         </AsideStyle>
     )
 }
--- src/components/aside/style.js
+++ src/components/aside/style.js
@@ -1,45 +1,111 @@
 import styled from "styled-components";
 
 export const AsideStyle = styled.aside`
-height: 100vh;
+  width: 25%;
+  height: 100vh;
+  padding-left: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
-a{
-display: block;
-height: 30px;
-width: 100px;
-margin-top: 10px;
-margin-left:160px;
-color: ${({theme})=>theme.preto};
-font-style: normal;
+  a {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 0.5rem;
+    height: 40px;
+    width: 80%;
+    color: ${({ theme }) => theme.preto};
+  }
 
-h3{
+  div {
+    .img-perfil {
+      width: 39px;
+      height: 39px;
+      left: 0px;
+      top: 10px;
+      border-radius: 99999px;
+      border: 1px solid black;
+    }
+  }
 
-}
+  .h3-perfil {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+  }
 
-h5{
-padding-top:0px ;
-color:#5B7083;
-font-size:13px;
-}
+  h3 {
+    font-weight: 400;
+    font-size: 0.8rem;
+  }
 
-}
+  .button-div {
+    width: 100%;
+    display: flex;
+    margin-top: 3rem;
 
+    button {
+      border: none;
+      width: 190px;
+      height: 40px;
+      border-radius: 2rem;
+      background-color: ${({ theme }) => theme.roxoEscuro};
+      color: white;
+      font-size: 1rem;
+      cursor: pointer;
 
-button{
-width: 227px;
-height: 49px;
-margin-top: 10px;
-margin-left:90px;
-border-radius: 9999px;
-margin-top: 50px;
-background-color:  ${({theme})=>theme.roxoEscuro} ;
-border: 0px;
-color:white;
-}
+      :hover {
+        opacity: 60%;
+      }
+    }
 
-.btn-perfil, .btn-inicio{
-    margin-top:10px;
-}
+    .btn-mobile{
+      display: none;
+    }
+  }
 
+  @media screen and (max-width: 580px) {
+  width: 15%; 
 
-`
+  padding-left: 0.2rem;
+  .h3-perfil,h3,h5{
+    display:none;
+  }
+   
+  div{
+    width: 80%;
+    display: flex;
+    justify-content: center;
+
+    .img-perfil{
+      width: 24px;
+      height: 24px;
+    }
+  }
+
+  a{
+    justify-content: center;
+  }
+
+  .button-div{
+
+   .btn-mobile{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5px;
+    width: 32px;
+    height: 32px;
+    display: block;
+    border-radius: 50%;
+   }
+
+   button{
+    display: none;
+   }
+  }
+
+  }
+`;
