6642a8e272cea1ee5e38c9a64699896677eaf9b0
sergiohdljr <sergio.tjf1@gmail.com>
2023-04-08 21:26:27 -0300

    fix/ modal bug

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { UseTema } from "./store";
 import { RoutesApp } from "./router/routes";
-import { ModalOcorrencia } from "./components/modal";
 
 export const App = () => {
   const atualTema = UseTema((state) => state.tema);
