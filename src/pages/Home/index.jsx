a55ac85fb160cb6e22e95f7848001464ac036ed5
sergiohdljr <sergio.tjf1@gmail.com>
2023-04-09 19:57:27 -0300

    feat/ adicioando label em ocorrencias no mapa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,12 +14,13 @@ import { UseSetModal } from "../../store";
 import usePlacesAutocomplete, { getGeocode } from "use-places-autocomplete";
 import { getLatLng } from "react-places-autocomplete";
 import { useState } from "react";
+import { PinterestLogo } from "@phosphor-icons/react";
 
 export const Map = () => {
   const position = { lat: -8.05428, lng: -34.8813 };
   const modalState = UseSetModal((state) => state.modal);
 
-  const [selected, setSelected] = useState()
+  const [selected, setSelected] = useState();
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: apiKey,
@@ -49,11 +50,27 @@ export const Map = () => {
       {modalState ? <ModalOcorrencia /> : null}
       <Aside />
       <PlacesAutoComplete setSelect={setSelected} />
-      {selected && <Marker position={selected} />}
+      {selected && (
+        <Marker
+          position={selected}
+          options={{
+            title: "você",
+            label:{
+              text: "você"
+            }
+          }}
+        />
+      )}
       {ocorrenciasMapa &&
         ocorrenciasMapa.map((ocorrencia) => (
           <Marker
             key={ocorrencia.id}
+            title={ocorrencia.descricaoDaOcorrencia}
+            options={{
+              label:{
+                text: ocorrencia.descricaoDaOcorrencia
+              }
+            }}
             position={{
               lat: parseFloat(ocorrencia.latitude),
               lng: parseFloat(ocorrencia.longitude),
@@ -63,6 +80,16 @@ export const Map = () => {
     </GoogleMap>
   ) : null;
 };
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="20"
+  height="20"
+  fill="#fafafa"
+  viewBox="0 0 256 256"
+>
+  <path d="Z"></path>
+</svg>;
 
 export const HomePage = () => {
   return <Map />;
@@ -77,24 +104,26 @@ const PlacesAutoComplete = ({ setSelect }) => {
     clearSuggestions,
   } = usePlacesAutocomplete();
 
-  const onSelected = async (endereco)=>{
-    setValue(endereco,false)
-    clearSuggestions()
+  const onSelected = async (endereco) => {
+    setValue(endereco, false);
+    clearSuggestions();
 
-    const address = endereco
-    const results = await getGeocode({address})
-    
+    const address = endereco;
+    const results = await getGeocode({ address });
+
     const { lat, lng } = await getLatLng(results[0]);
 
-    setSelect({lat, lng});
-  }
+    setSelect({ lat, lng });
+  };
 
   return (
-    <div style={{zIndex:90}}>
+    <div style={{ zIndex: 90 }}>
       <input
         type="text"
         placeholder="Digite sua localização"
         style={{
+          backgroundColor:"white",
+          color: "black",
           zIndex: 100,
           height: "40px",
           width: "200px",
@@ -104,10 +133,15 @@ const PlacesAutoComplete = ({ setSelect }) => {
         onChange={(e) => setValue(e.target.value)}
         disabled={!ready}
       />
-      <div style={{backgroundColor:"white"}} > 
-        {status === 'OK' && data.map(({place_id, description})=>{
-         return <p onClick={e=>onSelected(description)} key={place_id}>{description}</p>
-        })}
+      <div style={{ backgroundColor: "white" }}>
+        {status === "OK" &&
+          data.map(({ place_id, description }) => {
+            return (
+              <p onClick={(e) => onSelected(description)} key={place_id}>
+                {description}
+              </p>
+            );
+          })}
       </div>
     </div>
   );
