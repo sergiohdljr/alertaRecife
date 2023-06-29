c1eeb8db1a999c6acde6484da68012b786c0a3f2
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-29 17:13:56 -0300

    refactor: import w index on routes file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ import { FeedPage} from './Feed'
 import {HomePage } from './Home'
 import {Login } from './Login'
 import { PerfilPage } from './Perfil'
+
+export { FeedPage , HomePage , Login, PerfilPage}
