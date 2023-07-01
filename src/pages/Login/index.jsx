5de971e08b93f6bab4f33fe91d173ad39f879094
sergiohdljr <sergio.tjf1@gmail.com>
2023-07-01 15:17:05 -0300

    feat: login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,36 +1,23 @@
 import { LoginPage } from "./style";
-import logo_resized from "../../assets/logo_resized.png";
-import { GoogleLogo } from "@phosphor-icons/react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../service/firebase";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import logo from "../../assets/logo_resized.png";
+import { Form, FildSet, InputFild, ImageContainer, SubmitBtn, Label } from '../Register/style'
 
 export const Login = () => {
   
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    user ? localStorage.setItem("user", JSON.stringify(user)) : null;
-  }, [user]);
-
-  const handleGoogleSignIn = () => {
-    const Provider = new GoogleAuthProvider();
-    signInWithPopup(auth, Provider)
-      .then((result) => {
-        setUser(result.user);
-        location.reload();
-      })
-      .catch((error) => console.log(error));
-  };
-
   return (
     <LoginPage>
-      <img src={logo_resized} alt="logo-alerta-recife"/>
-        <Link onClick={handleGoogleSignIn}>
-          <GoogleLogo size={24} />
-          Entrar com o Google
-        </Link>
+      <img src={logo} alt="logo-alerta-recife" loading="lazy"/>
+      <Form>
+        <FildSet>
+          <Label>E-mail: </Label>
+          <InputFild type="text" />
+        </FildSet>
+        <FildSet>
+          <Label>Senha: </Label>
+          <InputFild type="password" />
+        </FildSet>
+        <SubmitBtn> Login </SubmitBtn>
+      </Form>
     </LoginPage>
   );
 };
